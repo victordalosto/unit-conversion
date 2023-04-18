6fee8b19b6e012bb6bef1cdf45deb96384ecac00
Victor Dalosto <victordalosto@gmail.com>
2023-04-18 11:17:43 -0300

    refactoring on code

--- src/main/java/dalosto/engineering/unitconversion/interfaces/UnitFormula.java
+++ src/main/java/dalosto/engineering/unitconversion/interfaces/UnitFormula.java
@@ -9,12 +9,13 @@ import dalosto.engineering.unitconversion.domain.UnitType;
 public interface UnitFormula {
 
 
-    /** Build another Unit to the International System of Units (SI).  */
+    /** Build another Unit in the International System of Units (SI).  */
     Unit buildUnitToSI(Unit unit);
 
 
-    /** Returns another Unit converhings this unit into another Unit Type. */
+    /** Returns another Unit converthing this unit into another Unit Type. */
     Unit buildUnitIntoAnotherType(Unit unit, UnitType anotherType);
+
 
     
 }
